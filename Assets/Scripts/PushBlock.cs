ca1604983aec426999caeab21ba5a010b009c06a
manuvikash <manuvikashs@gmail.com>
2023-09-26 15:05:00 +0530

    blockPusher - commited to be trained

--- Assets/Scripts/PushBlock.cs
+++ Assets/Scripts/PushBlock.cs
@@ -9,10 +9,12 @@ public class PushBlock : Agent
 {
     Rigidbody ball;
     private int currentStep = 0;
-    private int maxSteps = 15000;
+    private int maxSteps;
+    private Vector3 lastBlockPosition;
     void Start()
     {
         ball = GetComponent<Rigidbody>();
+        lastBlockPosition = Block.localPosition;
     }
 
     public Transform Block;
@@ -58,7 +60,7 @@ public class PushBlock : Agent
         }
         System.Random random = new System.Random();
         int[] blockPos = blockPosArr[random.Next(0, blockPosArr.Length)];
-        return new Vector3(blockPos[0], 0.1f, blockPos[1]);
+        return new Vector3(blockPos[0], 0.76f, blockPos[1]);
     }
     
      public override void OnEpisodeBegin()
@@ -91,32 +93,74 @@ public class PushBlock : Agent
         sensor.AddObservation(ball.velocity.z);
     }
 
-    public float forceMultiplier = 10;
+    public float forceMultiplier;
 
-    private void OnCollisionEnter(Collision col) {
-        if(col.gameObject.name == "Cube"){
-            // float collisionForce = col.relativeVelocity.magnitude;
-            SetReward(0.1f);
-            // Debug.Log("Collision with cube, reward = " + 0.01f  * collisionForce);
+    // private void OnCollisionEnter(Collision col) {
+    //     if(col.gameObject.name == "Cube"){
+    //         // float collisionForce = col.relativeVelocity.magnitude;
+    //         SetReward(0.1f);
+    //         // Debug.Log("Collision with cube, reward = " + 0.01f  * collisionForce);
+    //     }
+    // }
+
+    public void MoveAgent(ActionSegment<int> act)
+    {
+        var dirToGo = Vector3.zero;
+        var rotateDir = Vector3.zero;
+
+        var action = act[0];
+
+        switch (action)
+        {
+            case 1:
+                dirToGo = transform.forward * 1f;
+                break;
+            case 2:
+                dirToGo = transform.forward * -1f;
+                break;
+            case 3:
+                rotateDir = transform.up * 1f;
+                break;
+            case 4:
+                rotateDir = transform.up * -1f;
+                break;
+            case 5:
+                dirToGo = transform.right * -0.75f;
+                break;
+            case 6:
+                dirToGo = transform.right * 0.75f;
+                break;
         }
+        transform.Rotate(rotateDir, Time.fixedDeltaTime * 200f);
+        ball.AddForce(dirToGo * forceMultiplier);
     }
+
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
         // Actions, size = 2
         currentStep++;
-        SetReward(-1f/MaxStep);
-        Vector3 controlSignal = Vector3.zero;
-        controlSignal.x = actionBuffers.ContinuousActions[0];
-        controlSignal.z = actionBuffers.ContinuousActions[1];
-        ball.AddForce(controlSignal * forceMultiplier);
+        MoveAgent(actionBuffers.DiscreteActions);
+        SetReward(-0.1f/MaxStep);
+        // Debug.Log("Delay reward = " + -0.1/MaxStep);
+        // Vector3 controlSignal = Vector3.zero;
+        // controlSignal.x = actionBuffers.ContinuousActions[0];
+        // controlSignal.z = actionBuffers.ContinuousActions[1];
+        // ball.AddForce(controlSignal * forceMultiplier);
 
         // Rewards
         float distanceToTarget = Vector3.Distance(Block.transform.localPosition, MiniPlane.localPosition);
 
+        float oldDistancetoTarget = Vector3.Distance(lastBlockPosition, MiniPlane.localPosition);
         // Reached target
+
+        if(distanceToTarget < oldDistancetoTarget){
+            SetReward(0.1f * oldDistancetoTarget - distanceToTarget);
+        }
+        lastBlockPosition = Block.transform.localPosition;
         if (distanceToTarget < 5.64f)
         {
             SetReward(5.0f);
+            Debug.Log("Success Reward");
             EndEpisode();
         }
 
@@ -137,10 +181,31 @@ public class PushBlock : Agent
         // }
     }
 
+    // public override void Heuristic(in ActionBuffers actionsOut)
+    // {
+    //     var continuousActionsOut = actionsOut.ContinuousActions;
+    //     continuousActionsOut[0] = Input.GetAxis("Horizontal");
+    //     continuousActionsOut[1] = Input.GetAxis("Vertical");
+    // }
+
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        var continuousActionsOut = actionsOut.ContinuousActions;
-        continuousActionsOut[0] = Input.GetAxis("Horizontal");
-        continuousActionsOut[1] = Input.GetAxis("Vertical");
+        var discreteActionsOut = actionsOut.DiscreteActions;
+        if (Input.GetKey(KeyCode.D))
+        {
+            discreteActionsOut[0] = 3;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            discreteActionsOut[0] = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            discreteActionsOut[0] = 4;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            discreteActionsOut[0] = 2;
+        }
     }
 }
